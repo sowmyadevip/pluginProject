48918c039049f401fce5f321a36c2d98e0dba056
sowmya <prakashoct@gmail.com>
2016-05-31 13:23:49 +0530

    Added Sub file and modified Main.java file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ class Main
 public static void main(String args[])
 {
 Add.main(new String[0]);
+Sub.main(new String[0]);
 }
 }
---
+++ src/main/java/Sub.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Sub
+{
+public static void main(String[] args)
+{
+int a = 20;
+int b = 10;
+int s = a-b;
+System.out.println("The difference is:" +s); 
+}
+}
