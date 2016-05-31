82bd6e329d1af09fb64a63584e9edce4029847a3
sowmya <prakashoct@gmail.com>
2016-05-31 13:31:48 +0530

    Adding Mul Fileand edited Main.javafile

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public static void main(String args[])
 {
 Add.main(new String[0]);
 Sub.main(new String[0]);
+Mul.main(new String[0]);
 }
 }
---
+++ src/main/java/Mul.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Mul
+{
+public static void main(String[] args)
+{
+int a = 30;
+int b = 10;
+int p = a*b;
+System.out.println("The product is:" +p); 
+}
+}
